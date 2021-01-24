354f9fb4df78e895f3d977931274ec2542488fe1
Mateusz Grzybowinski <mateusz.grzybowinski@student.ap.be>
2021-01-24 23:11:06 +0100

    menuitems gemaakt met eigen logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,23 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 import PropTypes from "prop-types"
+import Menu from "./Menu"
+import {HeaderWrapper, Image} from './headerStyles/headerStyles'
 
 //zonder curly brackets kan je query niet gebruiken
 const Header = ({ siteTitle }) => {
   const {
+    logo,
       wpcontent: {menuItems},
   } = useStaticQuery(graphql`
   query{
+    logo: file(relativePath: {eq: "logo.png"}) {
+      childImageSharp {
+        fixed(quality: 100, width: 200){
+          ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
     wpcontent {
     menuItems {
       edges {
@@ -20,8 +30,12 @@ const Header = ({ siteTitle }) => {
   }
   }
   `)
-  console.log(menuItems, 'menu items')
-return <div>Header</div>
+return <HeaderWrapper>
+  <Link to="/">
+    <Image alt="logo cms taak" fixed={logo.childImageSharp.fixed}/>
+  </Link>
+  <Menu menuItems={menuItems.edges}/>
+</HeaderWrapper>
 }
 Header.propTypes = {
   siteTitle: PropTypes.string,
